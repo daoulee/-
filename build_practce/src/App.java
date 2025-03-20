845290c6ba2c34be7726a0660c718311a65546d5
daoulee <daoule@naver.com>
2025-03-20 12:12:21 +0900

    3/20일 수업

--- build_practce/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-     System.out.println("Hello, World!");
-    }
-}                     
-
-// 터미널 단축키 Command + control + '(백틱))'
-// 상단 터미널 단축키 Command + p
-// > java create ( 자바 새로운 파일 만들기)
